0eb412d244ff71bd15ad330a9d96ca73171761d3
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-11 18:00:24 -0300

    Added functionality for withdrawals and deposits

--- index.js
+++ index.js
@@ -25,18 +25,35 @@ const registrationSchema = joi.object({
     name: joi.string().min(3).max(30).required(),
     email: joi.string().email().min(5).max(50).required(),
     password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    repeatPassword: joi.ref('password'),
+    repeat_password: joi.ref('password'),
 });
 
-const loginSchema = joi.object({
-    email: joi.string().email().min(5).max(50).required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+const dataSchema = joi.object({
+    user_id: joi.required(),
+    date: joi.date().required(),
+    amount: joi.number().required(),
+    description: joi.string().min(1).max(20).required(),
+    transaction_type: joi.string().required()
 });
 
 //Tela de cadastro de usuário
 app.post('/sign-up', async (req, res) => {
 
-    const { name, email, password, repeatPassword } = req.body;
+    const { name, email, password } = req.body;
+
+    const validation = registrationSchema.validate(req.body);
+
+    if(validation.error) {
+        res.status(422).send(validation.error);
+        return;
+    }
+
+    const userExist = await db.collection('users').find({email}).count();
+
+    if(userExist > 0) {
+        res.status(409).send({"message": "Já existe usuário cadastrado nesse email!"});
+        return;
+    }{name, email}
 
     const hashPassword = bcrypt.hashSync(password, 10);
 
@@ -44,10 +61,10 @@ app.post('/sign-up', async (req, res) => {
        await db.collection('users').insert({
         name,
         email,
-        password: hashPassword,
-        repeatPassword: hashPassword,
+        password: hashPassword
        });
-       return res.send(201);
+       res.status(201).send({name, email});
+       return
     } catch (error) {
         console.error(error);
         return res.send(500)
@@ -67,13 +84,13 @@ app.post('/sign-in', async (req, res) => {
         const isValid = bcrypt.compareSync(password, user.password);
 
         if(!isValid) {
-            return res.send(401);
+            return res.status(401).send({"message": "Usuário ou senha inválidos!"});
         }
 
         const token = uuidv4();
         db.collection('sessions').insertOne({
             token,
-            userId: user._id,
+            user_id: user._id,
         });
         return res.send(token);
 
@@ -83,9 +100,26 @@ app.post('/sign-in', async (req, res) => {
     }
 });
 
+async function tokenVerification(token) {
+    const session = await db.collection('sessions').findOne({
+        token,
+    });
+
+    if(!session) {
+        return false
+    }
+
+    const user = await db.collection('users').findOne({
+        _id: session.user_id,
+    });
+
+    console.log("usuario encontrado: ", user)
+    return user
+}
+
 // rota de entrada após ter feito o login
 // rota privada
-app.get('/prohibited', async (req, res) => {
+app.get('/transactions', async (req, res) => {
 
     const token = req.headers.authorization?.replace('Bearer ', '');
 
@@ -93,30 +127,144 @@ app.get('/prohibited', async (req, res) => {
         return res.send(401);
     }
 
+    const user = await tokenVerification(token);
+
+    if(!user) {
+        res.send(401);
+    }
+
     try {
-        const session = await db.collection('sessions').findOne({
-            token,
-        });
-
-        if(!session) {
-            return res.send(401)
-        }
-
-        const user = await db.collection('users').findOne({
-            _id: session.userId,
-        });
-
-        const list = await db.collection('lists').find({
-            userId: user._id,
+        const list = await db.collection('bank_transactions').find({
+            user_id: user._id,
         }).toArray();
 
-        return res.send(list);
+        return res.status(200).send({list});
 
     } catch (error) {
         console.error(error);
+        return res.send(error)
     }
 
-    return res.send(200);
+});
+
+//Rota de deposito 
+app.post('/deposit', async (req, res) => {
+    const { description, amount} = req.body;
+
+    const token = req.headers.authorization?.replace('Bearer ', '');
+
+    if(!token) {
+        console.log("tOKEN NÃO ENVIADO: ", token)
+        return res.send(401);
+    }
+
+    const user = await tokenVerification(token);
+
+    if(!user) {
+        console.log("usuario não encontrado: ", user)
+        res.send(401);
+        return
+    }
+
+    try {
+        const inputDate = {
+            user_id: user._id,
+            date: new Date(),
+            description,
+            amount,
+            transaction_type: "deposito",
+        };
+
+        const validation = dataSchema.validate(inputDate, {abortEarly: false});
+
+        if(validation.error) {
+            res.status(422).send(validation.error);
+            return;
+        }
+
+        const depositDate = await db.collection('bank_transactions').insertOne({
+            ...inputDate
+        });
+
+        res.status(201).send(depositDate);
+        return
+
+    } catch (error) {
+        res.status(500).send(error);
+        return
+    }
+});
+
+// Rota de saque
+app.post('/withdraw', async (req, res) => {
+    const { description, amount} = req.body;
+
+    const token = req.headers.authorization?.replace('Bearer ', '');
+
+    if(!token) {
+        console.log("tOKEN NÃO ENVIADO: ", token)
+        return res.send(401);
+    }
+
+    const user = await tokenVerification(token);
+
+    if(!user) {
+        console.log("usuario não encontrado: ", user)
+        res.send(401);
+        return
+    }
+
+    try {
+        const userTransactions = await db.collection('bank_transactions').find({
+            user_id: user._id,
+        }).toArray();
+
+        let saldo = 0;
+
+        userTransactions.map((transaction) => {
+            if (transaction.transaction_type === "deposito"){
+                saldo = saldo + transaction.amount
+            } else if (transaction.transaction_type === "saque") {
+                saldo = saldo - transaction.amount
+            }
+        })
+
+        if(saldo < amount) {
+            res.status(401).send({"message": "Saldo insuficiente!"})
+            return;
+        }
+ 
+        const inputDate = {
+            user_id: user._id,
+            date: new Date(),
+            description,
+            amount,
+            transaction_type: "saque",
+        };
+
+        const validation = dataSchema.validate(inputDate, {abortEarly: false});
+
+        if(validation.error) {
+            res.status(422).send(validation.error);
+            return;
+        }
+
+        const depositDate = await db.collection('bank_transactions').insertOne({
+            ...inputDate
+        });
+
+        res.status(201).send(depositDate);
+        return
+
+    } catch (error) {
+        res.status(500).send(error);
+        return
+    }
+});
+
+//Log out
+app.delete('/logout', (req, res) => {
+    
 });
 
 app.listen(5000, () => console.log('App runnig in port 5000'));
